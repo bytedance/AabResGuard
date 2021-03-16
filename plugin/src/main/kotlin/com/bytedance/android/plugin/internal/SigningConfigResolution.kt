da221795bf2385c3569acf18b76bf97c5cfaaf87
wanglonghai <wanglonghai@bytedance.com>
2021-03-16 16:38:33 +0800

    fix:remove useless methods

--- plugin/src/main/kotlin/com/bytedance/android/plugin/internal/SigningConfigResolution.kt
+++ plugin/src/main/kotlin/com/bytedance/android/plugin/internal/SigningConfigResolution.kt
@@ -1,10 +1,8 @@
 package com.bytedance.android.plugin.internal
 
 import com.android.build.gradle.api.ApplicationVariant
-import com.android.build.gradle.internal.VariantManager
 import com.bytedance.android.plugin.model.SigningConfig
 import org.gradle.api.Project
-import java.io.File
 
 /**
  * Created by YangJing on 2020/01/06 .
@@ -37,43 +35,4 @@ private fun getSigningConfigForAGP4(agpVersion: String, project: Project, varian
 
 private fun getSigningConfigByAppVariant(variant: ApplicationVariant): SigningConfig {
     return SigningConfig(variant.signingConfig.storeFile, variant.signingConfig.storePassword, variant.signingConfig.keyAlias, variant.signingConfig.keyPassword)
-}
-
-/**
- * Return SigningConfig.
- * Range: 4.* to 4.0.0-alpha08
- */
-private fun getBuildTypesForAGPBefore4008(variantManager: VariantManager): Map<*, *>? {
-    return try {
-        variantManager::class.java.getMethod("getBuildTypes").invoke(variantManager) as Map<*, *>
-    } catch (e: Exception) {
-        return null
-    }
-}
-
-/**
- * Return SigningConfig.
- * Range: 4.0.0-alpha09 and after all.
- */
-private fun getBuildTypesForAGP4009(variantManager: VariantManager): Map<*, *>? {
-    return try {
-        val variantInputModelField = variantManager::class.java.getDeclaredField("variantInputModel")
-        variantInputModelField.isAccessible = true
-        val variantInputModel = variantInputModelField.get(variantManager)
-        val buildTypesField = variantInputModel::class.java.getField("buildTypes")
-        return buildTypesField.get(variantInputModel) as Map<*, *>?
-    } catch (e: Exception) {
-        null
-    }
-}
-
-
-private fun invokeSigningConfig(any: Any): SigningConfig {
-    val storeFile: File = any::class.java.getMethod("getStoreFile").invoke(any) as File
-    val keyAlias: String = any::class.java.getMethod("getKeyAlias").invoke(any) as String
-    val keyPassword: String = any::class.java.getMethod("getKeyPassword").invoke(any) as String
-    val storePassword: String = any::class.java.getMethod("getStorePassword").invoke(any) as String
-    return SigningConfig(
-            storeFile, storePassword, keyAlias, keyPassword
-    )
 }
